f7e1a0f744d080789d2781bb21c5f7accb5630a5
Clinton-odia <odiamehiclinton@gmail.com>
2026-05-05 13:52:37 +0100

    38 complete

--- src/018-unions-and-narrowing/059-narrowing-with-if-statements.problem.ts
+++ src/018-unions-and-narrowing/059-narrowing-with-if-statements.problem.ts
@@ -2,7 +2,8 @@ import { expect, it } from "vitest";
 
 function validateUsername(username: string | null): boolean {
   // Rewrite this function to make the error go away
-  return username.length > 5;
+
+  return username != null ? (username.length > 5 ? true : false) : false;
 
   return false;
 }
