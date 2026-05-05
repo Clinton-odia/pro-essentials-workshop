e7b286902b61c202ee4a3c70d3a8e34e1139a636
Clinton-odia <odiamehiclinton@gmail.com>
2026-05-05 14:28:43 +0100

    39 - 43 complete

--- src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
+++ src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
@@ -2,6 +2,9 @@ import { Equal, Expect } from "@total-typescript/helpers";
 
 const appElement = document.getElementById("app");
 
+if (!appElement) {
+  throw new Error("could not find app");
+}
 // How do I ensure that appElement is defined?
 
 type Test = Expect<Equal<typeof appElement, HTMLElement>>;
